7c9514b158ee7962a1d3a1e0b9b62af640282ec1
Konstantin Reffert <konstantin.reffert@student.englisches-institut.eu>
2026-03-04 15:14:59 +0100

    task 1-3

--- script.js
+++ script.js
@@ -2,10 +2,25 @@ document.getElementById('colorInput').addEventListener('input', function() {
     const color = this.value;
     document.getElementById('hexValue').textContent = color;
     document.getElementById('rgbValue').textContent = hexToRgb(color);
+
+    document.body.style.backgroundColor = color;
 });
+
 function hexToRgb(hex) {
     const r = parseInt(hex.slice(1, 3), 16);
     const g = parseInt(hex.slice(3, 5), 16);
     const b = parseInt(hex.slice(5, 7), 16);
     return `rgb(${r}, ${g}, ${b})`;
 }
+
+document.getElementById('copyHex').addEventListener('click', function() {
+    const hexText = document.getElementById('hexValue').textContent;
+    navigator.clipboard.writeText(hexText);
+    alert("HEX-Code " + hexText + " In Zwischenablage kopiert");
+});
+
+document.getElementById('copyRgb').addEventListener('click', function() {
+    const rgbText = document.getElementById('rgbValue').textContent;
+    navigator.clipboard.writeText(rgbText);
+    alert("RGB-Code " + rgbText + " In zwischenablage kopiert");
+});
